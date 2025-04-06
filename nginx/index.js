c1ba2e7b0cf55d2e7e07ff274cfc5108b9168db2
2wenty1ne <sirdarkvic@gmail.com>
2025-04-06 13:51:10 +0000

    Added function to serve html

--- nginx/index.js
+++ nginx/index.js
@@ -1,8 +1,10 @@
+const path = require('path')
 const express = require('express')
 const app = express()
 
-app.use("/", (req, res) =>  {
-    res.send("Server is running")
+app.use("/", function(req, res)  {
+    // res.send("Server is running")
+    res.sendFile(path.join(__dirname, '/content/test.html'))
 })
 
 app.listen(5000, console.log("Server started!"))
